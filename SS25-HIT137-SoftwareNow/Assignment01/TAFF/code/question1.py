a4eebd65ff82d258d91340252dd039844852c647
TafsirHarunTurjo <144098291+TafsirHarunTurjo@users.noreply.github.com>
2025-11-30 20:34:52 +0930

    Update question1.py

--- SS25-HIT137-SoftwareNow/Assignment01/TAFF/code/question1.py
+++ SS25-HIT137-SoftwareNow/Assignment01/TAFF/code/question1.py
@@ -1,1 +1,10 @@
-print('hello world')
+#getting user input password
+password = input("Enter your password: ")
+
+
+# print(password)
+
+#checking length of password
+length = len(password)
+
+print(length)
